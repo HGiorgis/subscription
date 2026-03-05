c8bcb121ec7abe83155c4eb55552da78170653c5
proxyH423 <hgiorgis23@proton.me>
2026-03-05 16:51:36 +0300

    media issue for render fix

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +12,13 @@ urlpatterns = [
     path('subscriptions/', include('subscriptions.urls')),
 ]
 
+# Serve media files in production
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # THIS IS KEY - Serve media files in production
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
